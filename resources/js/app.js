e074be997563205620ab9bcf4e415f76b9fe1181
Nisrina Asad <nisrina@nisrinas-macbook.local>
2026-04-10 14:05:11 +0700

    sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+window.toggleCRM = function() {
+    const menu = document.getElementById("crmMenu");
+    const arrow = document.getElementById("arrow");
+    const btn = document.getElementById("crmBtn");
+
+    if (menu && arrow && btn) {
+        menu.classList.toggle("hidden");
+        arrow.classList.toggle("rotate-180");
+        btn.classList.toggle("bg-[#142062]");
+    }
+};
