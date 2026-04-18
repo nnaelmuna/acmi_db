c2d6500ab0a1ffebc0ad17b2df2895827a3d4b35
Azka Syakirah <azkasyakir800@gmail.comA>
2026-04-18 19:36:52 +0700

    nambahin models buat bagian dashboard

---
+++ database/migrations/2026_04_18_095036_add_fields_to_member_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('member_requests', function (Blueprint $table) {
+            $table->string('mobile')->nullable();
+            $table->string('company')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('position')->nullable();
+            $table->string('company_url')->nullable();
+            $table->enum('status', ['review', 'approved', 'rejected'])->default('review')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('member_requests', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2026_04_18_095140_add_fields_to_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->string('mobile')->nullable();
+            $table->string('company')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('position')->nullable();
+            $table->string('company_url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+        });
+    }
+};
