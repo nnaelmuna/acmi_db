c4c9f7657448c4b4945fd053113487e9ee562e13
Azka Syakirah <azkasyakir800@gmail.comA>
2026-04-18 15:24:14 +0700

    benerin new post

--- resources/views/new-post.blade.php
+++ resources/views/new-post.blade.php
@@ -74,7 +74,7 @@
 
                     <div>
                         <h3 class="font-bold text-sm mb-3">Upload Image</h3>
-                        <div class="border border-gray-300 rounded-xl bg-white aspect-square flex flex-col items-center justify-center cursor-pointer hover:bg-gray-50 transition-all border-dashed border-2 group">
+                        <div class="border-2 border-gray-300 rounded-xl bg-white aspect-square flex flex-col items-center justify-center cursor-pointer hover:bg-gray-50 transition-all border-dashed group">
                             <div class="w-16 h-16 bg-gray-100 rounded-lg flex items-center justify-center mb-2 group-hover:bg-gray-200">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-400">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6.75a1.5 1.5 0 0 0-1.5-1.5H3.75a1.5 1.5 0 0 0-1.5 1.5v12.905a1.5 1.5 0 0 0 1.5 1.5Z" />
