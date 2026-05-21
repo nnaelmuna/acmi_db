bfdae1d15bc6428a36a75f67bdc2576c8f5e76d4
nnaelmuna <naelmuna2009@gmail.com>
2026-05-21 20:53:52 +0700

    members update

---
+++ app/Http/Controllers/CRM/MemberController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\CRM;
+
+use App\Http\Controllers\Controller;
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Member::query();
+
+        // Fitur pencarian untuk sub menu member
+        if ($request->filled('search')) {
+            $search = $request->search;
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                  ->orWhere('company_name', 'like', "%{$search}%")
+                  ->orWhere('email', 'like', "%{$search}%");
+            });
+        }
+
+        $members = $query->latest()->paginate(10)->withQueryString();
+
+        return view('crm.members.index', compact('members'));
+    }
+}
